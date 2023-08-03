2e7f6a05b7895637d6f7272d7c70980578f7e395
Irina Veselova <ladelaym@gmail.com>
2023-08-03 18:13:17 +0300

    feat: add solution in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  if(n > 0) {
+   return Number([...n.toString()].reverse().join(''));
+  } else {
+    const delSymbol = '-';
+    const str = n.toString();
+    const delSymbolStr = str.replace(delSymbol, '');
+
+    return Number([...delSymbolStr].reverse().join(''));
+  }
 }
